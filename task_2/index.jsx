ed980bf7ace8b73392f77e072e6891c4aae157d1
dicharmed <36915693+dicharmed@users.noreply.github.com>
2025-09-08 14:43:57 +0200

    solution for task 2

--- task_2/index.jsx
+++ task_2/index.jsx
@@ -1,16 +1,23 @@
-import { Fragment, memo } from 'react';
+import React, { Fragment, memo, useCallback } from "react";
 
-const MainComponent = () => {
-    const makeLog = () => console.log('hi from MainComponent'); // function to make logs from MainComponent
+type ChildProps = {
+  makeLog: () => void;
+};
 
-    return (
-        <Fragment>
-            <ChildComponent makeLog={makeLog} />
-        </Fragment>
-    );
+//без useCallback каждый раз создается новая функция = новая ссылка, что приводит к перерисовке ChildComponent
+const MainComponent: React.FC = () => {
+  const makeLog = useCallback(() => console.log("hi from MainComponent"), []); // function to make logs from MainComponent
+
+  return (
+    <Fragment>
+      <ChildComponent makeLog={makeLog} />
+    </Fragment>
+  );
 };
 
 // memoized component
-const ChildComponent = memo(({ makeLog }) => (
-    <button onClick={makeLog}>say Hi from ChildComponent</button>
+const ChildComponent = memo<ChildProps>(({ makeLog }) => (
+  <button onClick={makeLog}>say Hi from ChildComponent</button>
 ));
+
+export default MainComponent;
