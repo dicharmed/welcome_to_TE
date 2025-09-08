8008e484e62bd58025972525f6f5f41fc5a7e939
dicharmed <36915693+dicharmed@users.noreply.github.com>
2025-09-08 15:07:24 +0200

    solution for task 7

--- task_7/index.tsx
+++ task_7/index.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Button(props: { children: string }) {
-    const [disabled, setDisabled] = useState<boolean>();
+  const [disabled, setDisabled] = useState<boolean>();
 
-    return (
-        <button className='' onClick={() => setDisabled(true)} disabled={disabled}>
-            {props.children}
-        </button>
-    );
+  return (
+    <button
+      type="button"
+      className="rounded-lg px-3 py-1.5 text-white bg-blue-700 hover:bg-blue-500 disabled:bg-gray-400 disabled:cursor-not-allowed disabled:hover:bg-gray-400 transition-colors"
+      onClick={() => setDisabled(true)}
+      disabled={disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
